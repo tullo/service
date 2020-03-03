31008b1f79a6e40141654d09d0ce6c4575ce7ceb
Andreas Amstutz <andreasamstutz@gmail.com>
2020-03-03 19:35:34 +0100

    Enable trace ctx propagation
    Former-commit-id: dc671de8536adc1d975c89e7f3352dbb2bcdcc71

--- cmd/search/internal/handlers/search.go
+++ cmd/search/internal/handlers/search.go
@@ -10,6 +10,7 @@ import (
 	"github.com/tullo/service/cmd/search/internal/views"
 	"github.com/tullo/service/internal/platform/web"
 	"github.com/tullo/service/internal/product"
+	"go.opencensus.io/plugin/ochttp/propagation/tracecontext"
 	"go.opencensus.io/trace"
 )
 
@@ -38,12 +39,20 @@ func (s *Search) Query(ctx context.Context, w http.ResponseWriter, r *http.Reque
 		return err
 	}
 
+	// Create child span for backend call
+	ctx, childspan := trace.StartSpan(ctx, s.url)
+	defer childspan.End()
+
 	// Create a context with a timeout of 1 second.
 	ctx, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
 
 	// Bind the new context into the request.
 	req = req.WithContext(ctx)
+
+	// Add span context to backend call
+	format := &tracecontext.HTTPFormat{}
+	format.SpanContextToRequest(childspan.SpanContext(), req)
 
 	// Make the web call and return any error. Do will handle the
 	// context level timeout.
