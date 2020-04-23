07ae7f685fd5baa54941aa05f6a90c57ef121230
Andreas Amstutz <andreasamstutz@gmail.com>
2020-04-24 00:17:16 +0200

    allow only authenticated user to view products

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -42,9 +42,9 @@ func API(build string, shutdown chan os.Signal, log *log.Logger, db *sqlx.DB, au
 	p := Product{
 		db: db,
 	}
-	app.Handle(http.MethodGet, "/v1/products", p.List)
+	app.Handle(http.MethodGet, "/v1/products", p.List, mid.Authenticate(authenticator))
 	app.Handle(http.MethodPost, "/v1/products", p.Create, mid.Authenticate(authenticator))
-	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
+	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve, mid.Authenticate(authenticator))
 	app.Handle(http.MethodPut, "/v1/products/{id}", p.Update, mid.Authenticate(authenticator))
 	app.Handle(http.MethodDelete, "/v1/products/{id}", p.Delete, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
 
