4cb0eff8196e035d0f69cad2d3a31b73b3580382
Andreas Amstutz <andreasamstutz@gmail.com>
2020-08-26 15:40:47 +0200

    refactor tracer sampling configuration

--- foundation/tracer/tracer.go
+++ foundation/tracer/tracer.go
@@ -23,13 +23,15 @@ func Init(serviceName string, reporterURI string, probability float64, log *log.
 		return errors.Wrap(err, "creating new exporter")
 	}
 
-	// WARNING: The current Init settings are using defaults which I listed out
-	// for readability. Please review the documentation for opentelemetry.
+	// Demo mode configuarion, always record and sample.
+	sampler := sdktrace.AlwaysSample()
+	if probability < 1 {
+		// Production mode configuarion. A probability=0.05 means only 5% of
+		// tracing information will be exported to Zipkin.
+		sampler = sdktrace.ProbabilitySampler(.10)
+	}
 	tp, err := sdktrace.NewProvider(
-		// Demo mode configuarion, always sample.
-		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
-		// Production mode configuarion
-		// sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.ProbabilitySampler(.10)}),
+		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sampler}),
 		sdktrace.WithBatcher(exporter,
 			sdktrace.WithMaxExportBatchSize(sdktrace.DefaultMaxExportBatchSize),
 			sdktrace.WithBatchTimeout(sdktrace.DefaultBatchTimeout),
