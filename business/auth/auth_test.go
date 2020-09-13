55a967624be4bdb9e6460be6f812566922d64537
Andreas Amstutz <tullo@users.noreply.github.com>
2020-09-13 21:15:42 +0200

    refactor auth test

--- business/auth/auth_test.go
+++ business/auth/auth_test.go
@@ -10,58 +10,12 @@ import (
 	"github.com/tullo/service/business/auth"
 )
 
-func TestAuthenticator(t *testing.T) {
-
-	// Parse the private key used to generate the token.
-	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(privateRSAKey))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a key lookup function that returns the public key for the test KID.
-	keyLookupFunc := func(publicKID string) (*rsa.PublicKey, error) {
-		if publicKID != keyID {
-			return nil, errors.New("no public key found")
-		}
-		return &privateKey.PublicKey, nil
-	}
-	a, err := auth.New(privateKey, keyID, "RS256", keyLookupFunc)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	claims := auth.Claims{
-		StandardClaims: jwt.StandardClaims{
-			Issuer:    "service project",
-			Subject:   "0x01",
-			Audience:  "students",
-			ExpiresAt: time.Now().Add(8760 * time.Hour).Unix(),
-			IssuedAt:  time.Now().Unix(),
-		},
-		Roles: []string{"ADMIN"},
-	}
-
-	token, err := a.GenerateToken(claims)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	parsedClaims, err := a.ValidateToken(token)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if exp, got := len(claims.Roles), len(parsedClaims.Roles); exp != got {
-		t.Fatalf("expected %v roles, got %v", exp, got)
-	}
-	if exp, got := claims.Roles[0], parsedClaims.Roles[0]; exp != got {
-		t.Fatalf("expected roles[0] == %v, got %v", exp, got)
-	}
-}
+// How to generate a public key PEM file.
+// openssl rsa -pubout -in private.pem -out public.pem
 
 // The key id we would have generated for the keys below.
 // The key id represents the public key in the public key store.
-const keyID = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+const publicTestKID = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
 
 // Output of:
 // openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
@@ -94,5 +48,75 @@ E8Imrlt3vuxnUE1UMkhDXrlhrxslRXU9enynVghAcSrg6ijs8KuN/9RB/I7H03cT
 77mx9eHMcYcRUciY5C8AOaArmMA=
 -----END PRIVATE KEY-----`
 
-// How to generate a public key PEM file.
-// openssl rsa -pubout -in private.pem -out public.pem
+// Success and failure markers.
+const (
+	success = "\u2713"
+	failed  = "\u2717"
+)
+
+func TestAuth(t *testing.T) {
+	t.Log("Given the need to be able to authenticate and authorize access.")
+	{
+		testID := 0
+		t.Logf("\tTest %d:\tWhen handling a single user.", testID)
+		{
+			// Parse the private key used to generate the token.
+			privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(privateRSAKey))
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to parse the private key from pem: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to parse the private key from pem.", success, testID)
+
+			// Create a key lookup function that returns the public key for the test KID.
+			keyLookupFunc := func(publicKID string) (*rsa.PublicKey, error) {
+				if publicKID != publicTestKID {
+					return nil, errors.New("no public key found")
+				}
+				return &privateKey.PublicKey, nil
+			}
+
+			authenticator, err := auth.New(privateKey, publicTestKID, "RS256", keyLookupFunc)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to create an authenticator: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to create an authenticator.", success, testID)
+
+			claims := auth.Claims{
+				StandardClaims: jwt.StandardClaims{
+					Issuer:    "service project",
+					Subject:   "5cf37266-3473-4006-984f-9325122678b7",
+					Audience:  "students",
+					ExpiresAt: time.Now().Add(8760 * time.Hour).Unix(),
+					IssuedAt:  time.Now().Unix(),
+				},
+				Roles: []string{auth.RoleAdmin},
+			}
+
+			token, err := authenticator.GenerateToken(claims)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to generate a JWT: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to generate a JWT.", success, testID)
+
+			parsedClaims, err := authenticator.ValidateToken(token)
+			if err != nil {
+				t.Fatalf("\t%s\tTest %d:\tShould be able to parse the claims: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould be able to parse the claims.", success, testID)
+
+			if exp, got := len(claims.Roles), len(parsedClaims.Roles); exp != got {
+				t.Logf("\t\tTest %d:\texp: %d", testID, exp)
+				t.Logf("\t\tTest %d:\tgot: %d", testID, got)
+				t.Fatalf("\t%s\tTest %d:\tShould have the expexted number of roles: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould have the expexted number of roles.", success, testID)
+
+			if exp, got := claims.Roles[0], parsedClaims.Roles[0]; exp != got {
+				t.Logf("\t\tTest %d:\texp: %v", testID, exp)
+				t.Logf("\t\tTest %d:\tgot: %v", testID, got)
+				t.Fatalf("\t%s\tTest %d:\tShould have the expexted roles: %v", failed, testID, err)
+			}
+			t.Logf("\t%s\tTest %d:\tShould have the expexted roles.", success, testID)
+		}
+	}
+}
