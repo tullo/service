dfdd2d65ffb18e763b329a05d1e148d695412adb
Andreas Amstutz <tullo@users.noreply.github.com>
2021-03-29 22:19:33 +0200

    sales-admin: ensure db is ready for migration

--- app/sales-admin/commands/migrate.go
+++ app/sales-admin/commands/migrate.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/tullo/service/business/data/schema"
@@ -15,9 +17,16 @@ var ErrHelp = errors.New("provided help")
 func Migrate(cfg database.Config) error {
 	db, err := database.Open(cfg)
 	if err != nil {
-		return errors.Wrap(err, "connect database")
+		return errors.Wrap(err, "connect to database")
 	}
 	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return errors.Wrap(err, "database status check")
+	}
 
 	if err := schema.Migrate(db); err != nil {
 		return errors.Wrap(err, "migrate database")
