4f59856e9a264744d7fa2a07a29c651df99e08c5
Andreas Amstutz <tullo@users.noreply.github.com>
2021-04-20 11:01:32 +0200

    mid: add support for optional cors headers

---
+++ business/mid/cors.go
@@ -0,0 +1,35 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/tullo/service/foundation/web"
+	"go.opentelemetry.io/otel/trace"
+)
+
+// Cors sets the response headers needed for Cross-Origin Resource Sharing
+func Cors(origin string) web.Middleware {
+
+	// This is the actual middleware function to be executed.
+	m := func(handler web.Handler) web.Handler {
+
+		// Create the handler that will be attached in the middleware chain.
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			ctx, span := trace.SpanFromContext(ctx).Tracer().Start(ctx, "business.mid.cors")
+			defer span.End()
+
+			// Set the CORS headers on the response.
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, DELETE, GET, POST, PUT")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Encoding, Authorization, Content-Type, Content-Length, X-CSRF-Token")
+
+			// Call the next handler.
+			return handler(ctx, w, r)
+		}
+
+		return h
+	}
+
+	return m
+}
