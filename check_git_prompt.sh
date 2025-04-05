43fce96aa0ecd1f452be9115616cec0fdaae1e7c
Andreas Amstutz <tullo@users.noreply.github.com>
2025-04-06 01:36:15 +0200

    check if the directory exists

--- check_git_prompt.sh
+++ check_git_prompt.sh
@@ -2,15 +2,22 @@
 
 # Define the file path and the URL for downloading
 FILE_PATH="$HOME/.nix-profile/share/bash-completion/completions/git-prompt.sh"
+DIRECTORY_PATH="$(dirname "$FILE_PATH")"
 DOWNLOAD_URL="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+
+# Check if the directory exists
+if [ -d "$DIRECTORY_PATH" ]; then
+    echo "Directory $DIRECTORY_PATH already exists."
+else
+    echo "Directory $DIRECTORY_PATH not found. Creating it..."
+    mkdir -p "$DIRECTORY_PATH"
+fi
 
 # Check if the file exists
 if [ -f "$FILE_PATH" ]; then
     echo "git-prompt.sh already exists at $FILE_PATH"
 else
     echo "git-prompt.sh not found. Downloading..."
-    # Create the directory if it doesn't exist
-    mkdir -p "$(dirname "$FILE_PATH")"
     # Download the file
     curl -o "$FILE_PATH" "$DOWNLOAD_URL"
     echo "git-prompt.sh has been downloaded to $FILE_PATH"
