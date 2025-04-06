37a04b39943905f8a5c2ef04319df9e750c027e6
Andreas Amstutz <tullo@users.noreply.github.com>
2025-04-06 02:25:39 +0200

    rm devbox

--- check_git_prompt.sh
+++ check_git_prompt.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+#"bash check_git_prompt.sh",
+#". ~/.nix-profile/share/bash-completion/completions/git-prompt.sh",
+#"export PS1='\\[\\e[32m\\][\\u@\\h \\W$(__git_ps1 \" (%s)\")]\\$\\[\\e[0m\\] '",
+
+
 # Define the file path and the URL for downloading
 FILE_PATH="$HOME/.nix-profile/share/bash-completion/completions/git-prompt.sh"
 DIRECTORY_PATH="$(dirname "$FILE_PATH")"
 DOWNLOAD_URL="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
 
-ls -l /home/runner/.local/state/nix/profiles/profile/
 # Check if the directory exists
 if [ -d "$DIRECTORY_PATH" ]; then
     echo "Directory $DIRECTORY_PATH already exists."
